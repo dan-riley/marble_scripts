4316b426cde7358796673623fab71119d488d37e
Dan Riley <dan.riley@colorado.edu>
2021-09-08 16:56:20 -0600

    Updated mapping relay and test servers

--- base_station/run_darpa.sh
+++ base_station/run_darpa.sh
@@ -2,4 +2,5 @@
 trap 'kill %2; kill %3; kill %4' SIGINT
 cd ~/test_scoring_server
 docker-compose up --build &
-roslaunch mapping_server mapping_server.launch
+cd ~/base_station
+roslaunch mapping_server.launch
